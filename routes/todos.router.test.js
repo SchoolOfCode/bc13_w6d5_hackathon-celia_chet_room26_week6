8f8c0e55b65235eb97c2eca743ef9c58c4e70862
Celia Bocking <celiabocking99@gmail.com>
2022-11-04 14:28:13 +0000

    post

--- routes/todos.router.test.js
+++ routes/todos.router.test.js
@@ -15,4 +15,14 @@ describe('GET /api/todos', function() {
       expect(response.body).toStrictEqual({success:true, payload:expect.any(Array)})
 
     });
+
+it('test for new task added', async () => {
+  const data = {
+    task: "complete kata",
+    completionDate: "18/12/2022"
+  }
+const response = await request(app).post("/api/todos").send(data)
+expect(response.body).toStrictEqual({success: true, payload: data})
+})
+
   });
