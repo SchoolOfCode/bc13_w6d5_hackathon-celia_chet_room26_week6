6c6bdf50e8ebbf6b3edb2cdcf1767e828684c4fc
Celia Bocking <celiabocking99@gmail.com>
2022-11-04 10:33:36 +0000

    test create button

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
-describe('empty spec', () => {
+describe('testing website', () => {
   it('passes', () => {
     cy.visit('localhost:3000')
   })
+it('adding todo', ()=>{
+  cy.get('[data-cy="task-input"]').type('this is a test task')
+  cy.get('[data-cy="date-input"]').type('2022-01-23')
+  cy.get('[data-cy="create-button"]').click()
+  
 })
+})
+
+
+
+
